86b98f2c6bf89f40c022c25b0a83e4d39860dfb7
juju8899 <146299878+juju8899@users.noreply.github.com>
2023-10-25 04:33:08 +0000

    Bookers2-debug-sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root :to =>"homes#top"
+  root to: "homes#top"
   get "home/about"=>"homes#about"
   devise_for :users
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
