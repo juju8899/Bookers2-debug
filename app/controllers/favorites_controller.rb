631f3f0976ab87d29b37bfce2047ae7c5ba5e583
juju8899 <146299878+juju8899@users.noreply.github.com>
2023-10-28 05:00:51 +0000

    Bookers2/いいね機能

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,7 @@
 class FavoritesController < ApplicationController
   def create
     book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(booke_id: book.id)
+    favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
     redirect_to books_path
   end
