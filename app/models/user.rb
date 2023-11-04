e95864f30e11ee6acd376c71997f92de40ef3ea3
juju8899 <146299878+juju8899@users.noreply.github.com>
2023-11-04 11:38:49 +0000

    Bookers2-debug/検索機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,22 @@ class User < ApplicationRecord
   def following?(user)
     followed.find_by(follower_id: user.id).present?
   end
+  
+  def self.looks(search, word)
+    if search == "perfect_matcch"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?", "#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?", "%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?", "%#{word}%")
+    else
+      @user = User.all
+    end
+  end
+      
+      
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
