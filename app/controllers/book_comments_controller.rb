0209611f12db331e84386c6698fb4945b243ef01
juju8899 <146299878+juju8899@users.noreply.github.com>
2023-11-06 16:08:38 +0000

    Bookers2-debug/非同期通信化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,15 +1,15 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
+    render :book_comments
   end
 
   def destroy
-    BookComment.find(params[:id]).destroy
-    redirect_to request.referer
+    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    render :book_comments
   end
 
   private
