a9161b9928b296590ae9735388b52e5c3a263715
juju8899 <146299878+juju8899@users.noreply.github.com>
2023-11-06 14:11:31 +0000

    Bookers2-debug/非同期通信化

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,7 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
